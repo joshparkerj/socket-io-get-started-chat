517f540ba9143800fe7a1e142088361a49da0556
Joshua Parker <10247779@uvu.edu>
2018-11-17 15:25:44 -0700

    run linter

--- index.js
+++ index.js
@@ -2,34 +2,34 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.get('/index.css', (req,res) => {
+app.get('/index.css', (req, res) => {
     res.sendFile(__dirname + '/index.css');
 })
 
-io.on('connection',socket => {
+io.on('connection', socket => {
     // console.log(socket);
     socket.nickname = 'anonymous coward';
-    socket.broadcast.emit('user connected',socket.nickname);
+    socket.broadcast.emit('user connected', socket.nickname);
     console.log(`${socket.nickname} connected`);
-    socket.on('typing',() => {
-        socket.broadcast.emit('typing message',socket.nickname);
+    socket.on('typing', () => {
+        socket.broadcast.emit('typing message', socket.nickname);
         console.log(`${socket.nickname} is typing...`)
     })
-    socket.on('chat message',msg => {
-        socket.broadcast.emit('chat message',JSON.stringify({n: socket.nickname, m: msg}));
+    socket.on('chat message', msg => {
+        socket.broadcast.emit('chat message', JSON.stringify({ n: socket.nickname, m: msg }));
         console.log(`${socket.nickname}: ${msg}`);
     })
-    socket.on('change name',nickname => {
-        socket.broadcast.emit('name change',JSON.stringify({old: socket.nickname, new: nickname}));
+    socket.on('change name', nickname => {
+        socket.broadcast.emit('name change', JSON.stringify({ old: socket.nickname, new: nickname }));
         console.log(`${socket.nickname} changed name to: ${nickname}`);
         socket.nickname = nickname;
     })
-    socket.on('disconnect',() => {
-        io.emit('user disconnected',socket.nickname);
+    socket.on('disconnect', () => {
+        io.emit('user disconnected', socket.nickname);
         console.log(`${socket.nickname} disconnected`);
     })
 })
